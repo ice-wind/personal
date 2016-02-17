ba559106e06650870d5aa06b664e6790aae3c004
Roman <romanwarga@gmail.com>
2016-02-17 15:47:50 +0100

    spyscroll , fixed menu

--- gallery/js/init.js
+++ gallery/js/init.js
@@ -9,9 +9,23 @@ $(document).ready(function(){
    $(window).scroll(function(){
       if ($(this).scrollTop() > 120) {
          $('.btnToTop').fadeIn();
+		 $('.leftMenuLine').animate({opacity:1},200);
       } else {
          $('.btnToTop').fadeOut();
+		 $('.leftMenuLine').animate({opacity:0},100);
       }
    });
+/*---------------scroll to selected section---------------*/
+	
+	$('.sectionOne').on('click',function(){
+		var section = $('#sectionOne').offset();
+		$('html,body').animate({scrollTop:section.top},800);
+		return false;
+	});
+	$('.sectionTwo').on('click',function(){
+		var section = $('#sectionTwo').offset();
+		$('html,body').animate({scrollTop:section.top},800);
+		return false;
+	});
    
 });
