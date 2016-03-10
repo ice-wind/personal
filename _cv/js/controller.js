2fe84b3902ecd586e12e85315eabf460b2be749b
Roman <romanwarga@gmail.com>
2016-03-10 15:00:45 +0100

    cv add header, work on content structure

--- _cv/js/controller.js
+++ _cv/js/controller.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+	$('.circle').hover(function(){
+		$(this).children('i').css('color','#679cb9');
+	},function(){
+		$(this).children('i').css('color','grey');
+	});
+	
 	
 });
