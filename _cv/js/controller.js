d6905976f246059bc105ab1f362bf683fd4cae2a
Roman <romanwarga@gmail.com>
2016-03-17 14:54:00 +0100

    add icons , animate

--- _cv/js/controller.js
+++ _cv/js/controller.js
@@ -5,5 +5,38 @@ $(document).ready(function(){
 		$(this).children('i').css('color','grey');
 	});
 	
+	$('.box').hover(function(){
+		$(this).children('p').css('opacity','0');
+	},function(){
+		$(this).children('p').css('opacity','1');
+	});
+	
+	$('.box').hover(function(){
+
+		$(this).children('.titleHoverIn').addClass('abc');
+	},function(){
+		$(this).children('.titleHoverIn').removeClass('abc');
+	});
+	
 	
 });
+
+$(window).scroll(function() {
+	if(scrollOn($('.progress'))){
+		$('.progress-bar-language').css('width','80%');
+	}
+	
+	
+		
+	
+})
+
+function scrollOn(element){
+	var bottomOfObject = (element).outerHeight()+(element).offset().top;
+	var bottomOfWindow = $(window).height() + $(window).scrollTop();
+	if(bottomOfObject<bottomOfWindow){
+		return true;
+	}else{
+		return false;
+	}
+}
